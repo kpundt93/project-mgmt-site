06707479c4d2da478c96de7544b41aa6b7e9b99d
Katie Pundt <kzpundt@gmail.com>
2022-03-02 20:53:46 -0800

    add Signup form and styles

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,12 +1,66 @@
 import React from 'react';
-
+import { useState } from 'react'
 // styles
 import './Signup.css'
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName, thumbnail);
+  }
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (!selected) {
+      setThumbnailError('Please select a file');
+      return;
+    }
+
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image');
+      return;
+    }
+
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100kb');
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+    console.log('thumbnail updated');
+  }
+
   return (
-    <div>
-      Signup
-    </div>
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      <label>
+        <span>email:</span>
+        <input type='email' onChange={(e) => setEmail(e.target.value)} value={email} required />
+      </label>
+      <label>
+        <span>password:</span>
+        <input type='password' onChange={(e) => setPassword(e.target.value)} value={password} required />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input type='text' onChange={(e) => setDisplayName(e.target.value)} value={displayName} required />
+      </label>
+      <label>
+        <span>profile thumbnail:</span>
+        <input type='file' onChange={handleFileChange} required />
+        {thumbnailError && <div className='error'>{thumbnailError}</div>}
+      </label>
+      <button className='btn'>Sign Up</button>
+    </form>
   )
 }
