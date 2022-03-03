d70d77b31b62ecbb16fa01a27af2059f17ca4076
Katie Pundt <kzpundt@gmail.com>
2022-03-02 18:53:27 -0800

    set up and initialize firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxBf8L0IpJyeS-s0vVeS_Uyzj0mrIw5UM",
+  authDomain: "thedojosite-ff2ed.firebaseapp.com",
+  projectId: "thedojosite-ff2ed",
+  storageBucket: "thedojosite-ff2ed.appspot.com",
+  messagingSenderId: "222801718315",
+  appId: "1:222801718315:web:2a4e992636ea59381ece6e"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
