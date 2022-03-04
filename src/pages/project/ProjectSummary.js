48043b11fe22dff338708d8ac117b8001134893e
Katie Pundt <kzpundt@gmail.com>
2022-03-04 15:51:14 -0800

    add ability to delete projects

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useHistory } from 'react-router-dom'
+// components
 import Avatar from '../../components/Avatar'
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore('projects');
+  const { user } = useAuthContext();
+  const history = useHistory();
+  
+  const handleClick = () => {
+    deleteDocument(project.id);
+    history.push('/');
+  };
+
   return (
     <div>
       <div className='project-summary'>
         <h2 className='page-title'>{project.name}</h2>
+        <p>By: {project.createdBy.displayName}</p>
         <p className='due-date'>Project due by: {project.dueDate.toDate().toDateString()}</p>
         <p className='details'>{project.details}</p>
         <h4>Project is assigned to:</h4>
@@ -17,6 +31,9 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button className='btn' onClick={handleClick}>Mark as Complete</button>
+      )}
     </div>
   )
 }
