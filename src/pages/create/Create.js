ee673820d0cc17696b28c153982505f546981cc3
Katie Pundt <kzpundt@gmail.com>
2022-03-04 11:54:15 -0800

    add error messages for select dropdowns

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -21,6 +21,7 @@ export default function Create() {
   const [dueDate, setDueDate] = useState('');
   const [category, setCategory] = useState('');
   const [assignedUsers, setAssignedUsers] = useState([]);
+  const [formError, setFormError] = useState(null);
 
   useEffect(() => {
     if (documents) {
@@ -33,6 +34,18 @@ export default function Create() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError(null);
+
+    if (!category) {
+      setFormError('Please select a project category.');
+      return;
+    }
+
+    if (assignedUsers.length < 1) {
+      setFormError('Please assign the project to at least one user.');
+      return;
+    }
+
     console.log(name, details, dueDate, category.value, assignedUsers);
   }
 
@@ -61,6 +74,7 @@ export default function Create() {
           <Select onChange={(option) => setAssignedUsers(option)} options={users} isMulti />
         </label>
         <button className='btn'>Add Project</button>
+        {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   )
