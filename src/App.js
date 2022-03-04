8dd6b3159bb55ac45905eed35c0ca623d811d07c
Katie Pundt <kzpundt@gmail.com>
2022-03-03 18:37:04 -0800

    add redirects and conditional rendering in navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 // pages & components
 import Dashboard from './pages/dashboard/Dashboard'
 import Create from './pages/create/Create'
@@ -12,31 +13,40 @@ import Sidebar from './components/Sidebar'
 import './App.css'
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-      <Sidebar />
-        <div className="container">
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Dashboard />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path='/projects/:id'>
-              <Project />
-            </Route>
-            <Route path='/login'>
-              <Login />
-            </Route>
-            <Route path='/signup'>
-              <Signup />
-            </Route>
-          </Switch>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+        <Sidebar />
+          <div className="container">
+            <Navbar />
+            <Switch>
+              <Route exact path="/">
+                {!user && <Redirect to='/login' />}
+                {user && <Dashboard />}
+              </Route>
+              <Route path="/create">
+                {!user && <Redirect to='/login' />}
+                {user && <Create />}
+              </Route>
+              <Route path='/projects/:id'>
+                {!user && <Redirect to='/login' />}
+                {user && <Project />}
+              </Route>
+              <Route path='/login'>
+                {user && <Redirect to='/' />}
+                {!user && <Login />}
+              </Route>
+              <Route path='/signup'>
+                {user && <Redirect to='/' />}
+                {!user && <Signup />}
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks//useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 // styles
 import './Navbar.css'
 // images
@@ -8,6 +9,7 @@ import Temple from '../assets/temple.svg'
 
 export default function Navbar() {
   const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div className='navbar'>
@@ -16,12 +18,20 @@ export default function Navbar() {
           <img src={Temple} alt='dojo logo' />
           <span>The Dojo</span>
         </li>
-        <li><Link to='/login'>Login</Link></li>
-        <li><Link to='/signup'>Signup</Link></li>
-        <li>
-          {!isPending && <button className='btn' onClick={logout}>Logout</button>}
-          {isPending && <button className='btn' disabled>Logging out...</button>}
-        </li>
+        {/* show links if no user is logged in */}
+        {!user && (
+          <>
+            <li><Link to='/login'>Login</Link></li>
+            <li><Link to='/signup'>Signup</Link></li>
+          </>
+        )}
+        {/* show button when user is logged in */}
+        {user && (
+          <li>
+            {!isPending && <button className='btn' onClick={logout}>Logout</button>}
+            {isPending && <button className='btn' disabled>Logging out...</button>}
+          </li>
+        )}
       </ul>
     </div>
   )
