b9b63f1f166053f2ef6599dce4eb70c07634bd5b
Katie Pundt <kzpundt@gmail.com>
2022-03-04 10:05:01 -0800

    add users sidebar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Signup from './pages/signup/Signup'
 import Project from './pages/project/Project'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import OnlineUsers from './components/OnlineUsers'
 // styles
 import './App.css'
 
@@ -45,6 +46,7 @@ function App() {
               </Route>
             </Switch>
           </div>
+          {user && <OnlineUsers />}
         </BrowserRouter>
       )}
     </div>
---
+++ src/components/OnlineUsers.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useCollection } from '../hooks/useCollection'
+//components
+import Avatar from './Avatar'
+// styles
+import './OnlineUsers.css';
+
+export default function OnlineUsers() {
+  const { isPending, error, documents } = useCollection('users');
+
+  return (
+    <div className='user-list'>
+      <h2>All Users</h2>
+      {isPending && <div>Loading users...</div>}
+      {error && <div className='error'>{error}</div>}
+      {documents && documents.map(user => (
+        <div key={user.id} className='user-list-item'>
+          <span>{user.displayName}</span>
+          <Avatar src={user.photoURL} />  
+        </div>
+      ))}
+    </div>
+  )
+}
