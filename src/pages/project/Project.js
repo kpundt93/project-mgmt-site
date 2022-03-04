2bb9908c0ea4979ad257af9f7b7b37ad10724d91
Katie Pundt <kzpundt@gmail.com>
2022-03-04 14:46:22 -0800

    create and display project summary

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
+import ProjectSummary from './ProjectSummary'
 // styles
 import './Project.css'
 
 export default function Project() {
+  const { id } = useParams();
+  const { error, document } = useDocument('projects', id);
+
+  if (error) {
+    return <div className='error'>{error}</div>;
+  }
+
+  if (!document) {
+    return <div className='loading'>Loading...</div>;
+  }
+
   return (
-    <div>
-      Project details
+    <div className='project-details'>
+      <ProjectSummary project={document} />
     </div>
   )
 }
