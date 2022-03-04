bd755355f008f8455a4734cfb81b9185cf3d4386
Katie Pundt <kzpundt@gmail.com>
2022-03-03 18:16:12 -0800

    implement login functionality

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from 'react'
+import { useState } from 'react'
+import { useLogin } from '../../hooks/useLogin'
 // styles
 import './Login.css'
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, error, isPending } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  }
+
   return (
     <div>
-      Login
+      <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input type='email' onChange={(e) => setEmail(e.target.value)} value={email} required />
+      </label>
+      <label>
+        <span>password:</span>
+        <input type='password' onChange={(e) => setPassword(e.target.value)} value={password} required />
+      </label>
+      {!isPending && <button className='btn'>Login</button>}
+      {isPending && <button className='btn' disabled>Loading...</button>}
+      {error && <div className='error'>{error}</div>}
+    </form>
     </div>
   )
 }
