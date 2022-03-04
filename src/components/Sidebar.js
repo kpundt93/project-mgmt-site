226115fd145aa4ca01756ac584448dec899016a8
Katie Pundt <kzpundt@gmail.com>
2022-03-03 19:34:32 -0800

    add user greeting and avatar img

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
+// components
+import Avatar from './Avatar'
 // styles
 import './Sidebar.css'
 // images
@@ -7,12 +10,14 @@ import DashboardIcon from '../assets/dashboard_icon.svg'
 import AddIcon from '../assets/add_icon.svg'
 
 export default function Sidebar() {
+  const { user } = useAuthContext();
+
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          {/* avatar and username here later */}
-          <p>Hello</p>
+          <Avatar src={user.photoURL} />
+          <p>Hello, {user.displayName}</p>
         </div>
         <nav className="links">
           <ul>
